b3afe361b78cc277008c9e67c7346aa0e11dc60c
Felix Huiskamp <37748@ma-web.nl>
2023-11-21 15:29:59 +0100

    indexjs

--- 01A/app.js
+++ 01A/app.js
@@ -5,6 +5,20 @@ class App
     {
         console.log("hello world!");
         //code gaat hier!
+        let appNaam ="HelloWeb"
+        let versienummer ="0.1"
+        let versiedatum ="21-11-2023"
+        let autheur ="Felix Huiskamp"
+        let copyright ="FelixHuiskamp"
+        let distributeur ="Microsoft"
+        let darkmode =false 
+        console.log(appNaam)
+        console.log(versienummer)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
     }
 }
 
