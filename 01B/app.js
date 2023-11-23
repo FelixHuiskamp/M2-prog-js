5b5fa2e7d015116a4c2794ff76e6f39530037a1c
Felix Huiskamp <37748@ma-web.nl>
2023-11-23 14:47:49 +0100

    web

---
+++ 01B/app.js
@@ -0,0 +1,33 @@
+class App
+{
+
+    runApplication()
+    {
+        console.log("hello world!");
+        //code gaat hier!
+        this.greeting ="Starting - up";
+        this.appNaam ="HelloWeb"
+        this.versienummer ="0.1"
+        this.versiedatum ="21-11-2023"
+        this.autheur ="Felix Huiskamp"
+        this.copyright ="FelixHuiskamp"
+        this.distributeur ="Microsoft"
+        this.darkmode =false 
+        // console.log(this.greeting)
+        // console.log(this.appNaam)
+        // console.log(versienummer)
+        // console.log(versiedatum)
+        // console.log(autheur)
+        // console.log(copyright)
+        // console.log(distributeur)
+        // console.log(darkmode)
+    }
+}
+
+let app = new App();
+app.runApplication();
+
+
+console.log(app.greeting);
+console.log("appNaam: " + app.appNaam);
+console.log("versienummer: " + app.versienummer);
