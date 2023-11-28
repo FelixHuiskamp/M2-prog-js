af7b738f104b1a42607c8bbaf990794f9a711ed4
Felix Huiskamp <37748@ma-web.nl>
2023-11-28 14:27:21 +0100

    message

---
+++ 03 ifelse/app.js
@@ -0,0 +1,14 @@
+class App
+{
+
+    runApplication()
+    {
+        console.log("hello world!");
+        //code gaat hier!
+       //Je past de code aan met "Hidden" en "Style"
+       //Je gaat de code dan weer opvragen met "Id" en "Class"
+    }
+}
+
+let app = new App();
+app.runApplication();
