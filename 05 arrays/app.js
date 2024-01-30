9cab4173b7214eb78777979acc77a0ab780b6290
Felix Huiskamp <37748@ma-web.nl>
2024-01-30 13:56:59 +0100

    message

--- 05 arrays/app.js
+++ 05 arrays/app.js
@@ -12,6 +12,26 @@ class App
             const element = artiesten [i];
             console.log(i + ": "+ element)
         }
+
+        artiesten.push("bob dylan")
+        console.log(artiesten)
+
+        artiesten.push("prince")
+        console.log(artiesten)
+
+        for  (let i = 0; i < artiesten.length; i++) {
+            const element = artiesten [i];
+            console.log(i + ":"+ element);
+        }
+
+        let indexToRemove = artiesten.indexOf("bob dylan")
+        artiesten.splice(indexToRemove,1)
+        console.log(artiesten)
+
+        let indexToRemove2 = artiesten.indexOf("prince")
+        artiesten.splice(indexToRemove2,1)
+        console.log(artiesten)
+
         let nummers = [2, 5, 7]
         console.log(nummers)    
 
